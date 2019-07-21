715789eb45b440cb4c456cd8d972c4553d4296c7
PriyaC <priyacjose@gmail.com>
2019-07-21 09:44:07 -0500

    this is my trial version

---
+++ src/main/java/com/hzn/HARStrialVersion/HarsTrialVersionApplication.java
@@ -0,0 +1,13 @@
+package com.hzn.HARStrialVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarsTrialVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarsTrialVersionApplication.class, args);
+	}
+
+}
